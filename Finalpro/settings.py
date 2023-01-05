8051f94b8f95e2629a2868add1a52b78e4d426ec
Tyne14x <thanakrit.3360@gmail.com>
2023-01-05 20:08:33 +0700

    mail1

--- Finalpro/settings.py
+++ Finalpro/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 LOGIN_URL = 'login'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nutaponza123456@gmail.com'
-EMAIL_HOST_PASSWORD = 'yrqigshegrzsjcyj'
+EMAIL_HOST_USER = 'isoconsult.online.io@gmail.com'
+EMAIL_HOST_PASSWORD = 'kepjorgfdghhobux'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
